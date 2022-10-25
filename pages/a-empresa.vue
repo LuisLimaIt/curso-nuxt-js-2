4b5af10d3c5f7dc5a268e2581eb8072690f4463c
Luis Paulo de Lima Soares <pls.luis@gmail.com>
2022-10-25 15:07:08 -0300

    Layouts

--- pages/a-empresa.vue
+++ pages/a-empresa.vue
@@ -1,18 +1,8 @@
 <template>
     <div>
-        <div class="bg-gray-100">
-            <div class="container mx-auto flex items-center space-x-4 h-10 mb-10">
-                <NuxtLink :to="{ name: 'a-empresa' }">A empresa</NuxtLink>
-                <NuxtLink to="/servicos">Serviços</NuxtLink>
-                <NuxtLink to="/produtos">Produtos</NuxtLink>
-            </div>
-        </div>
-
         <div>
-            <div class="container mx-auto">
-                <h1 class="text-lg font-bold">A empresa</h1>
-                <p>Mussum Ipsum, cacilds vidis litro abertis. Viva Forevis aptent taciti sociosqu ad litora torquent.Copo furadis é disculpa de bebadis, arcu quam euismod magna.Quem num gosta di mim que vai caçá sua turmis!Mais vale um bebadis conhecidiss, que um alcoolatra anonimis.</p>
-            </div>
+            <h1 class="text-lg font-bold">A empresa</h1>
+            <p>Mussum Ipsum, cacilds vidis litro abertis. Viva Forevis aptent taciti sociosqu ad litora torquent.Copo furadis é disculpa de bebadis, arcu quam euismod magna.Quem num gosta di mim que vai caçá sua turmis!Mais vale um bebadis conhecidiss, que um alcoolatra anonimis.</p>
         </div>
     </div>
     
--- pages/produtos/index.vue
+++ pages/produtos/index.vue
@@ -1,20 +1,10 @@
 <template>
     <div>
-        <div class="bg-gray-100">
-            <div class="container mx-auto flex items-center space-x-4 h-10 mb-10">
-                <NuxtLink :to="{ name: 'a-empresa' }">A empresa</NuxtLink>
-                <NuxtLink to="/servicos">Serviços</NuxtLink>
-                <NuxtLink to="/produtos">Produtos</NuxtLink>
-            </div>
-        </div>
-
         <div>
-            <div class="container mx-auto">
-                <h1 class="text-lg font-bold">Produtos</h1>
-                <NuxtLink to="/produtos/produto-a">Produto A</NuxtLink>
-                <NuxtLink to="/produtos/produto-b">Produto B</NuxtLink>
-                <p>Mussum Ipsum, cacilds vidis litro abertis. Viva Forevis aptent taciti sociosqu ad litora torquent.Copo furadis é disculpa de bebadis, arcu quam euismod magna.Quem num gosta di mim que vai caçá sua turmis!Mais vale um bebadis conhecidiss, que um alcoolatra anonimis.</p>
-            </div>
+            <h1 class="text-lg font-bold">Produtos</h1>
+            <NuxtLink to="/produtos/produto-a">Produto A</NuxtLink>
+            <NuxtLink to="/produtos/produto-b">Produto B</NuxtLink>
+            <p>Mussum Ipsum, cacilds vidis litro abertis. Viva Forevis aptent taciti sociosqu ad litora torquent.Copo furadis é disculpa de bebadis, arcu quam euismod magna.Quem num gosta di mim que vai caçá sua turmis!Mais vale um bebadis conhecidiss, que um alcoolatra anonimis.</p> 
         </div>
     </div>
     
