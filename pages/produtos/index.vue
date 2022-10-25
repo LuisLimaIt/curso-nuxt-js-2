9e481946aaaf98139d6a09f49a087a6ec7cb9000
Luis Paulo de Lima Soares <pls.luis@gmail.com>
2022-10-25 17:59:52 -0300

    Busca de dados asyncData

--- pages/produtos/index.vue
+++ pages/produtos/index.vue
@@ -4,6 +4,16 @@
             <h1 class="text-lg font-bold">Produtos</h1>
             <NuxtLink to="/produtos/produto-a">Produto A</NuxtLink>
             <NuxtLink to="/produtos/produto-b">Produto B</NuxtLink>
+            <br>
+            <br>
+
+            <div 
+            v-for="product in products" :key="product.id"
+            class="border-b boder-gray-400 py-3"
+            >
+                {{ product.title }}
+            </div>
+
             <p>Mussum Ipsum, cacilds vidis litro abertis. Viva Forevis aptent taciti sociosqu ad litora torquent.Copo furadis é disculpa de bebadis, arcu quam euismod magna.Quem num gosta di mim que vai caçá sua turmis!Mais vale um bebadis conhecidiss, que um alcoolatra anonimis.</p> 
         </div>
     </div>
@@ -18,6 +28,15 @@
             return {
 
             };
+        },
+
+        async asyncData({ $axios }) {
+            
+            const products = await $axios.$get('https://jsonplaceholder.typicode.com/posts?_limit=3')
+
+            return {
+                products,
+            }
         },
 
         methods: {},
